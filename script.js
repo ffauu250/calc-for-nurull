7b83aa52502722914f6fc93c645fe892191e5fb9
ffauu250 <muhammadfauzanbambangmuharram@gmail.com>
2024-07-10 18:25:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function appendToDisplay(value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    const expression = display.value;
+    
+    // This will replace the result of any mathematical operation with "i love youu"
+    if (expression.match(/[0-9]+[+\-*/][0-9]+/)) {
+        display.value = 'i love youu';
+    } else {
+        try {
+            display.value = eval(expression);
+        } catch {
+            display.value = 'Error';
+        }
+    }
+}
+
+function clearDisplay() {
+    const display = document.getElementById('display');
+    display.value = '';
+}
